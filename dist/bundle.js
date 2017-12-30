78c3d521099d8cf944d85a65a6e4df786ae5a7d0
tojuhaka <tojuhaka@gmail.com>
2017-12-31 00:28:39 +0200

    hardcoded fetch added for testing

--- dist/bundle.js
+++ dist/bundle.js
@@ -17294,35 +17294,44 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 var coinPie = "coinPie";
 var seligsonPie = "seligsonPie";
 
+const url = "http://91.157.151.61/";
+
+const test = fetch(url)
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) {
+        var test = new __WEBPACK_IMPORTED_MODULE_1_chart_js___default.a(coinPie,{
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: [10, 20, 30, 12,34,45,67,45],
+                    borderWidth: 2,
+                    backgroundColor:[
+                        "#ff6384",
+                        "#ff6384",
+                        "#ff6384"
+                    ]
+                }],
+
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: [
+                    'Red',
+                    'Yellow',
+                    'Blue'
+                ]
+            },
+
+            options: {
+                color: ['red', 'blue', 'green'],
+                labels: {
+                    defaultFontSize: 8
+                }
+            }
+        });
+        // Create and append the li's to the ul
+})
+
+
 // For a pie chart
-var coinPieChart = new __WEBPACK_IMPORTED_MODULE_1_chart_js___default.a(coinPie,{
-    type: 'pie',
-    data: {
-        datasets: [{
-            data: [10, 20, 30, 12,34,45,67,45],
-            borderWidth: 2,
-            backgroundColor:[
-                "#ff6384",
-                "#ff6384",
-                "#ff6384"
-            ]
-        }],
-
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Red',
-            'Yellow',
-            'Blue'
-        ]
-    },
-
-    options: {
-        color: ['red', 'blue', 'green'],
-        labels: {
-            defaultFontSize: 8
-        }
-    }
-});
 
 var seligsonPieChart = new __WEBPACK_IMPORTED_MODULE_1_chart_js___default.a(seligsonPie,{
     type: 'pie',
--- src/index.js
+++ src/index.js
@@ -5,35 +5,44 @@ import css from './simple-grid.min.css';
 var coinPie = "coinPie";
 var seligsonPie = "seligsonPie";
 
+const url = "http://91.157.151.61/";
+
+const test = fetch(url)
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) {
+        var test = new Chart(coinPie,{
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: [10, 20, 30, 12,34,45,67,45],
+                    borderWidth: 2,
+                    backgroundColor:[
+                        "#ff6384",
+                        "#ff6384",
+                        "#ff6384"
+                    ]
+                }],
+
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: [
+                    'Red',
+                    'Yellow',
+                    'Blue'
+                ]
+            },
+
+            options: {
+                color: ['red', 'blue', 'green'],
+                labels: {
+                    defaultFontSize: 8
+                }
+            }
+        });
+        // Create and append the li's to the ul
+})
+
+
 // For a pie chart
-var coinPieChart = new Chart(coinPie,{
-    type: 'pie',
-    data: {
-        datasets: [{
-            data: [10, 20, 30, 12,34,45,67,45],
-            borderWidth: 2,
-            backgroundColor:[
-                "#ff6384",
-                "#ff6384",
-                "#ff6384"
-            ]
-        }],
-
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Red',
-            'Yellow',
-            'Blue'
-        ]
-    },
-
-    options: {
-        color: ['red', 'blue', 'green'],
-        labels: {
-            defaultFontSize: 8
-        }
-    }
-});
 
 var seligsonPieChart = new Chart(seligsonPie,{
     type: 'pie',
