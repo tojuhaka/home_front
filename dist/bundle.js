b9277c500e7873c9e1a320dfefdbbe0cc03396aa
tojuhaka <tojuhaka@gmail.com>
2017-12-31 17:16:42 +0200

    colors added + example authentication

--- dist/bundle.js
+++ dist/bundle.js
@@ -17294,22 +17294,38 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 var coinPie = "coinPie";
 var seligsonPie = "seligsonPie";
 
-const url = "http://91.157.151.61/";
+var default_colors = ['#3366CC','#DC3912','#FF9900','#109618','#990099','#3B3EAC','#0099C6','#DD4477','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC']
 
-const test = fetch(url)
+const url = "http://91.157.151.61:80/coins";
+
+function authenticateUser(user, password)
+{
+    var token = user + ":" + password;
+
+    // Should i be encoding this value????? does it matter???
+    // Base64 Encoding -> btoa
+    var hash = btoa(token);
+
+    return "Basic " + hash;
+}
+
+const headers = new Headers()
+headers.append('Authorization', authenticateUser("test", "test"))
+headers.set('origin', '*')
+
+const init = { headers: headers}
+
+const test = fetch(url, init)
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) {
+        console.log(data)
         var test = new __WEBPACK_IMPORTED_MODULE_1_chart_js___default.a(coinPie,{
             type: 'pie',
             data: {
                 datasets: [{
                     data: [10, 20, 30, 12,34,45,67,45],
                     borderWidth: 2,
-                    backgroundColor:[
-                        "#ff6384",
-                        "#ff6384",
-                        "#ff6384"
-                    ]
+                    backgroundColor: default_colors
                 }],
 
                 // These labels appear in the legend and in the tooltips when hovering different arcs
@@ -17339,11 +17355,7 @@ var seligsonPieChart = new __WEBPACK_IMPORTED_MODULE_1_chart_js___default.a(seli
         datasets: [{
             data: [10, 20, 30],
             borderWidth: 2,
-            backgroundColor:[
-                "#ff6384",
-                "#ff6384",
-                "#ff6384"
-            ]
+            backgroundColor: default_colors
         }],
 
         // These labels appear in the legend and in the tooltips when hovering different arcs
