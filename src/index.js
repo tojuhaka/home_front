71e520f79e7413f32805a736581204de716a344c
tojuhaka <tojuhaka@gmail.com>
2018-01-01 00:34:22 +0200

    pie chart added with percentages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import Chart from 'chart.js'
 import css from './simple-grid.min.css'
+import 'chart.piecelabel.js'
 
 var coinPie = "coinPie";
 var seligsonPie = "seligsonPie";
@@ -28,29 +29,50 @@ const init = { headers: headers}
 const test = fetch(url, init)
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) {
-        console.log(data)
+        const coin_values = []
+        const coin_labels = []
+        for (let i in data.coins.details) {
+           coin_values.push(data.coins.details[i].amount)
+           coin_labels.push(data.coins.details[i].id)
+        }
+
         var test = new Chart(coinPie,{
             type: 'pie',
             data: {
                 datasets: [{
-                    data: [10, 20, 30, 12,34,45,67,45],
+                    data: coin_values,
                     borderWidth: 2,
                     backgroundColor: default_colors
                 }],
 
                 // These labels appear in the legend and in the tooltips when hovering different arcs
-                labels: [
-                    'Red',
-                    'Yellow',
-                    'Blue'
-                ]
+                labels: coin_labels
             },
 
             options: {
+                pieceLabel: {
+                    render: function (args) {
+                        return args.label + ' ' + args.percentage +'%';
+                    },
+                    fontSize: 11,
+                    fontColor: '#fff'
+                },
                 color: ['red', 'blue', 'green'],
-                labels: {
-                    defaultFontSize: 8
-                }
+                tooltips: {
+                    callbacks: {
+                        label: function(tooltipItem, data) {
+                            var allData = data.datasets[tooltipItem.datasetIndex].data;
+                            var tooltipLabel = data.labels[tooltipItem.index];
+                            var tooltipData = allData[tooltipItem.index];
+                            var total = 0;
+                            for (var i in allData) {
+                                total += allData[i];
+                            }
+                            var tooltipPercentage = Math.round((tooltipData / total) * 100);
+                            return tooltipLabel + ': ' + Math.round(tooltipData) + ' (' + tooltipPercentage + '%)';
+                        }
+                    }
+                },
             }
         });
         // Create and append the li's to the ul
